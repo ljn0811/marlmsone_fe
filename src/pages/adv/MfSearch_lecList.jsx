6d46d29cfb9c73a2f92366383e8fa7b390004a93
goyeonjin <jinyjgo@gmail.com>
2024-05-17 11:36:01 +0900

    라우터 주소 추가

---
+++ src/pages/adv/MfSearch_lecList.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import {useState, useEffect} from 'react'
+
+
+
+/*강의 목록 조회*/
+export default function MfSearch_lecList(props){
+        console.log('props를 보고 싶어',props)  
+        const [lecList,setLecList] = useState([]); 
+        const [lectId, setLectId] = useState('');
+
+        useEffect(()=>{
+                     
+                allLec();
+        },[]);
+
+        //강의 목록 조회
+        const allLec = () => {
+                axios.get('/adv/mlecList.do').then((r)=>{
+                        setLecList(r.data.listData);
+                        setLectId(props.lecId);
+                });
+        }
+
+        //강의 선택 onchage
+        const selectLec=(e) => {
+                setLectId(e.target.value);
+                props.id(lectId);
+        }
+        return(
+                
+                <select onChange={selectLec} value={props.lecId} defaultValue={''}>
+                    <option name="lec_name" value="">전체</option>
+                 {
+                       lecList.map((a, i)=>{
+                        return ( 
+                                
+                                 <option name="lec_name" key={i} value={a.lec_id}>{a.lec_name}</option>
+                                )
+                          }) 
+                 }   
+                </select>)
+}
+
+
